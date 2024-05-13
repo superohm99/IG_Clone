b0003a2219a565a0bd56311e4f83597a7f27417c
Teeruth Ieowsakulrat <teeruth2546@gmail.com>
2024-05-13 19:52:38 +0700

    save Search

--- client/app/(tabs)/SearchScreen.tsx
+++ client/app/(tabs)/SearchScreen.tsx
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import SearchBox from '@/components/SearchPage/SearchBox'
+import SearchContent from '@/components/SearchPage/SearchContent'
 
 const SearchScreen = () => {
+  const [image, setImage] = useState<string | null>(null); // Specify the type as string or null
+
+  const getData = (data: string) => { // Explicitly specify the type of 'data' as string
+    setImage(data);
+  };
+
   return (
-    <View>
-      <Text>SearchScreen</Text>
+    <View style={styles.content}>
+      <ScrollView>
+        <SearchBox/>
+        <SearchContent data={getData}/>
+      </ScrollView>
     </View>
   )
 }
 
 export default SearchScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  content:{
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'white',
+    position:'relative'
+  },
+})
---
+++ client/components/SearchPage/SearchUser.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const SearchUser = () => {
+  return (
+    <View>
+      <Text>SearchUser</Text>
+    </View>
+  )
+}
+
+export default SearchUser
