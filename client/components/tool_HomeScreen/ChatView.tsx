660f782f9a0c1b08ec1a842b1139028b1c8f4936
Thanasak Songsri <Thanasuksongsriohm@gmail.com>
2024-05-20 02:19:10 +0700

    .

--- client/components/tool_HomeScreen/ChatView.tsx
+++ client/components/tool_HomeScreen/ChatView.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useMemo, useRef, useState } from 'react'
 import BottomSheet from '@gorhom/bottom-sheet';
 import CircleView from './CircleView';
 import CommentView from './CommentView';
+import { useMediaQuery } from 'react-responsive';
 
 var max_width = Dimensions.get('screen').width;
 var max_height = Dimensions.get('screen').height;
@@ -20,7 +21,7 @@ const ChatView = (props:SwitchChat) => {
   const [modalVisible, setModalVisible] = useState(false);
   const [draggedValue, setDraggedValue] = useState(0);
   const bottomSheetRef = useRef<BottomSheet>(null);
-  const snapPoints = useMemo(() => ['25%', '80%', '90%'], []);
+  const snapPoints = useMemo(() => ['25%', '60%', '70%'], []);
 
 
     useEffect(() => {
@@ -48,23 +49,38 @@ const ChatView = (props:SwitchChat) => {
       }
       setDraggedValue(0);
     };
+
+    const isTabletOrMobileDevice = useMediaQuery({
+      query: "(max-device-width: 1020px)",
+    });
+  
+    var margin_bottom:number = 50;
+    var margin_bottom_inside:number = 10
+    var left_comment:any = '48%' 
+    if (isTabletOrMobileDevice)
+      {
+        margin_bottom = 100
+        margin_bottom_inside = 60
+        left_comment='42%'
+      }
   return (
     <GestureHandlerRootView style={{flex:1}}>
     <Modal
             transparent={true}
             visible={modalVisible}
             onRequestClose={handleCloseModal}
+            
             >
             <View  style={{
               width:max_width,
               height:max_height,
               backgroundColor: 'rgba(0, 0, 0, 0.5)'}}>
 
-            <BottomSheet index={1} snapPoints={snapPoints}        ref={bottomSheetRef}>
+            <BottomSheet index={1} snapPoints={snapPoints}    ref={bottomSheetRef}>
                     
-                    <View style={{position:'relative',left:'42%',width:120,height:30}}>
+                    <View style={{position:'relative',left:left_comment,width:120,height:30}}>
 
-                        <Text>COMMENT</Text>
+                        <Text style={{fontWeight:600}}>COMMENT</Text>
 
                     </View>
 
@@ -84,8 +100,8 @@ const ChatView = (props:SwitchChat) => {
                         <CommentView></CommentView>
               
                         
-                        <TouchableOpacity onPress={handleCloseModal} style={{flex:1,flexDirection:'row',justifyContent:'center',marginBottom:100}}>
-                          <View style={{ marginTop: 10,width:120,height:30,marginBottom:60,backgroundColor:'black',borderWidth:2,borderColor:'black',borderRadius:7}}>
+                        <TouchableOpacity onPress={handleCloseModal} style={{flex:1,flexDirection:'row',justifyContent:'center',marginBottom:margin_bottom}}>
+                          <View style={{ marginTop: 10,width:120,height:30,marginBottom:margin_bottom_inside,backgroundColor:'black',borderWidth:2,borderColor:'black',borderRadius:7}}>
                               <Text style={{color: 'white',textAlign:'center'}}>BACK</Text>
                           </View>
                         </TouchableOpacity>
