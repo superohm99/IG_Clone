f1eec2a6e4ac3c87d901e07ccb314f0a4b3c1cf7
Tanathip Pona <nm3207petchza@gmail.com>
2024-06-22 01:43:33 +0700

    fix story functions

--- server/repository/story/story_db.go
+++ server/repository/story/story_db.go
@@ -16,6 +16,39 @@ func NewStoryRepositoryDB(db *gorm.DB) StoryRepository {
 	return storyRepositoryDB{db: db}
 }
 
+func (r storyRepositoryDB) GetAll() ([]models.Story, error) {
+	var stories []models.Story
+
+	result := r.db.Find(&stories)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return stories, nil
+}
+
+func (r storyRepositoryDB) GetOnlyFollowing(c *gin.Context) ([]models.Story, error) {
+	var stories []models.Story
+	var user models.User
+
+	activeUser, _ := c.Get("user")
+	r.db.Preload("Closed_friend").Find(&user, activeUser.(models.User).Id)
+	following := user.Closed_friend
+
+	for _, friend := range following {
+		var friendStories []models.Story
+		result := r.db.Where("user_id = ?", friend.Id).Find(&friendStories)
+		if result.Error != nil {
+			return nil, result.Error
+		}
+
+		stories = append(stories, friendStories...)
+	}
+
+	return stories, nil
+}
+
 func (r storyRepositoryDB) GetByUserId(id string) ([]models.Story, error) {
 	var stories []models.Story
 
